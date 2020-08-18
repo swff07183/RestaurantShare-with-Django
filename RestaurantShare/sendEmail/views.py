607485309223ef2d1eb914cdc06b74c7961c3275
swff07183 <swff07183@gmail.com>
2020-08-18 15:46:05 +0900

    sendEmail modify:

--- RestaurantShare/sendEmail/views.py
+++ RestaurantShare/sendEmail/views.py
@@ -5,6 +5,8 @@ from shareRes.models import *
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from django.core.mail import send_mail, EmailMessage
+from django.template.loader import render_to_string
 
 # Create your views here.
 def sendEmail(request) :
@@ -12,34 +14,18 @@ def sendEmail(request) :
     inputReceiver = request.POST['inputReceiver']
     inputTitle = request.POST['inputTitle']
     inputContent = request.POST['inputContent']
+    restaurants=[]
 
-    mail_html = "<html><body>"
-    mail_html += "<h1> 맛집 공유 </h1>"
-    mail_html += "<p>" + inputContent + "<br>"
-    mail_html += "발신자님께서 공유하신 맛집은 다음과 같습니다.</p>"
     for checked_res_id in checked_res_list :
-        restaurant = Restaurant.objects.get(id = checked_res_id)
-        mail_html += "<h2>"+restaurant.restaurant_name+"</h3>"
-        mail_html += "<h4>* 관련 링크</h4>" + "<p>" + restaurant.restaurant_link+"</p><br>"
-        mail_html += "<h4>* 상세 내용</h4>" + "<p>" + restaurant.restaurant_content+"</p><br>"
-        mail_html += "<h4>* 관련 키워드</h4>" + "<p>" + restaurant.restaurant_keyword + "</p><br>"
-        mail_html += "<br>"
+        restaurants.append(Restaurant.objects.get(id = checked_res_id))
 
-    mail_html += "</body></html>"
-    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    content = {'inputContent' : inputContent, 'restaurants' : restaurants}
 
-    # 구글 아이디 패스워드
-    server.login("hongsm0526@gmail.com", "ghdtjdahr12!")
+    msg_html = render_to_string('sendEmail/email_format.html',content)
 
-    msg = MIMEMultipart('alternative')
-    msg['Subject'] = inputTitle
-    msg['From'] = "hongsm0526@gmail.com"
-    msg['To'] = inputReceiver
-    mail_html = MIMEText(mail_html, 'html')
-    msg.attach(mail_html)
-    print(msg['To'], type(msg['To']))
-    server.sendmail(msg['From'],msg['To'].split(','), msg.as_string())
-    server.quit()
+    msg = EmailMessage(subject = inputTitle, body=msg_html, from_email="hongsm0526@gmail.com",bcc=inputReceiver.split(','))
+    msg.content_subtype = 'html'
+    msg.send()
 
     return HttpResponseRedirect(reverse('index'))
     # return HttpResponse("sendEmail")
